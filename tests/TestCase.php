0c714794dbaf720a74b80b8e5c6f088beca390cb
tabuna <tabuna@users.noreply.github.com>
2022-03-07 12:59:18 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -68,7 +68,7 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
     /**
      * Override application aliases.
      *
-     * @param  \Illuminate\Foundation\Application  $app
+     * @param \Illuminate\Foundation\Application $app
      *
      * @return array
      */
