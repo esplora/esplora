ecb3fcc272f9b61b8efe57976ff66e6d06d0d640
tabuna <bliz48rus@gmail.com>
2023-04-22 02:37:11 +0300

    Added new rules

---
+++ src/Rules/ExceptPath.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Esplora\Tracker\Rules;
+
+class ExceptPath implements Rule
+{
+    /**
+     * @param Request  $request
+     * @param Response $response
+     *
+     * @return bool
+     */
+    public function passes($request, $response): bool
+    {
+        $except = config('esplora.except', []);
+
+        if (empty($except)) {
+            return true;
+        }
+
+        return !$request->is($except);
+    }
+}
